2555270cb6fbfb3cb12f0104bf9b105aeea3322b
tuxalin <tux_alin@yahoo.com>
2017-11-20 20:33:30 +0200

    updated compile shader script and cmake cleanup

--- examples/compile_shaders.bat
+++ examples/compile_shaders.bat
@@ -1,3 +1,9 @@
 @ECHO OFF >NUL
 
-for /R %cd% %%f in (*.vert,*.frag) do %VULKAN_SDK%/Bin32/glslangValidator.exe -V %%~ff -o %%~ff.spv
+if "%~1" == "" (set WORK_PATH=%cd%) else (set WORK_PATH=%~1)
+
+echo.
+echo "Vulkan SDK path: "%VULKAN_SDK%
+echo "Compiling shaders in: "%WORK_PATH%
+echo.
+for /R %WORK_PATH% %%f in (*.vert,*.frag) do %VULKAN_SDK%/Bin32/glslangValidator.exe -V %%~ff -o %%~ff.spv
